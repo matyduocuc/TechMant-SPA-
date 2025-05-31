0049a6ee833d610b0df68f927a979650d7818e47
maty <mati.gaetet@duocuc.cl>
2025-05-31 14:49:55 -0400

    Add files via upload

---
+++ gestion-de-repuestos/src/main/java/com/example/gestion_de_repuestos/GestionDeRepuestosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeRepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeRepuestosApplication.class, args);
+	}
+
+}
