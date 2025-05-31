229e1814a533ab9d8a1cd33dbbff57525bdfd583
maty <mati.gaetet@duocuc.cl>
2025-05-31 14:00:00 -0400

    Add files via upload

---
+++ gestion-de-reportes/src/test/java/com/example/gestion/de/reportes/GestionDeReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion.de.reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeReportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
