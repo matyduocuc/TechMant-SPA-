bd868f1896a554cbf8371f66252362366e961510
maty <mati.gaetet@duocuc.cl>
2025-06-01 19:14:58 -0400

    Add files via upload

---
+++ gestion-de-equipos/src/test/java/com/example/gestion_de_equipos/GestionDeEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_de_equipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
