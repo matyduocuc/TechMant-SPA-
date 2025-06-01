dba5eeb659b19b1bc3e4b54aafec663ffc191d4c
maty <mati.gaetet@duocuc.cl>
2025-06-01 19:50:48 -0400

    Add files via upload

---
+++ gestion-servicios-tecnicos/src/test/java/com/example/gestion_servicios_tecnicos/GestionServiciosTecnicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_servicios_tecnicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionServiciosTecnicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
