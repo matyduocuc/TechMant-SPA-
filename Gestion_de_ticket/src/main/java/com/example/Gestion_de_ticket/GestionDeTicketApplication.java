e0b547ff66990717d6760bb874266989cb5c808e
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-06-10 10:25:48 -0400

    Add files via upload

---
+++ Gestion_de_ticket/src/main/java/com/example/Gestion_de_ticket/GestionDeTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTicketApplication.class, args);
+	}
+
+}
---
+++ Gestion_de_ticket/src/test/java/com/example/Gestion_de_ticket/GestionDeTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion_de_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
