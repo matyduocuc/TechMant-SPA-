e16c93b70455f0dc22df9dad38e30ca0ef338f0f
maty <mati.gaetet@duocuc.cl>
2025-06-19 17:01:16 -0400

    Add files via upload

---
+++ gestion_ventas/src/test/java/com/example/gestion_ventas/GestionVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestion_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
