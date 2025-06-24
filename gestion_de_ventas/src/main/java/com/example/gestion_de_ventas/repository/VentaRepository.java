c2a43b7bc3d62add8479eaaf7f7aedb23403a8bb
Ignaciodrx908 <ig.londono@duocuc.cl>
2025-06-24 18:35:49 -0400

    Delete gestion_de_ventas directory

--- gestion_de_ventas/src/main/java/com/example/gestion_de_ventas/repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gestion_de_ventas.repository;
-
-import com.example.gestion_de_ventas.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
